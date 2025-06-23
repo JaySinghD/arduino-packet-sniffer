a7ec3312ad85bb0c47d6be8caa8e2ccf2a23ca88
Jay Singh Dadhiala <109878620+JaySinghD@users.noreply.github.com>
2025-06-23 14:50:31 -0400

    Create esp32-sniffer.ino
    Created esp32-sniffer.ino and added the basic structure of the code

---
+++ esp32-sniffer.ino
@@ -0,0 +1,12 @@
+//Required Libraries
+#include <WiFi.h>
+#include <esp_wifi.h>
+
+void setup() {
+    Serial.begin(115200); //initializing the serial port and setting baud rate
+}
+
+
+void loop() {
+  
+}
